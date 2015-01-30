e4482b494ae67f4452c0f6b91618a6792e407a5d
Akrem Saed <akrem.saed@gmail.com>
2015-01-29 22:08:55 -0600

    connect to local mongodb and read a collection

---
+++ ASaed.Poc.MongoDb.Data/ioc/MongoConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+
+namespace ASaed.Poc.MongoDb.Data.ioc
+{
+    public class MongoConnectionFactory
+    {
+        public static MongoDatabase GetMongoDatabase()
+        {
+            var mongoClientSettings = new MongoClientSettings();
+            mongoClientSettings.Credentials = new[] { MongoCredential.CreateCredential("admin", "poc_csharp", "password") };
+            mongoClientSettings.Server = new MongoServerAddress("localhost", 27017);
+
+            var mongoClient = new MongoClient(mongoClientSettings);
+
+            return mongoClient.GetServer().GetDatabase("db-poc");
+        }
+    }
+}
---
+++ Asaed.Poc.MongoDb.Data.Test/ioc/MongoConnectionFactoryTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using ASaed.Poc.MongoDb.Data.ioc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using NUnit.Framework;
+
+namespace Asaed.Poc.MongoDb.Data.Test.ioc
+{
+    [TestFixture]
+    public class MongoConnectionFactoryTest
+    {
+
+        [Test]
+        public void ShouldConnectToMongoDatabase()
+        {
+            MongoDatabase mongoDatabase = MongoConnectionFactory.GetMongoDatabase();
+
+            Assert.IsNotNull(mongoDatabase);
+            MongoCollection<BsonDocument> mongoCollection = mongoDatabase.GetCollection("students");
+            Assert.IsNotNull(mongoCollection);
+
+            var asQueryable = mongoCollection.AsQueryable();
+            foreach (var bsonDoc in asQueryable)
+            {
+                Console.Write(string.Format("student = {0}", bsonDoc["name"]));
+            }
+        }
+    }
+}
